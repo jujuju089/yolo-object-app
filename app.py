655748176194952986840db4c7c5cbaf303c30f0
jujuju089 <schmitju@katharineum.de>
2026-03-16 14:58:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,6 @@ def index():
             filename = file.filename
 
     return render_template("index.html", result=result, filename=filename)
-
+    
 if __name__ == "__main__":
-app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False
