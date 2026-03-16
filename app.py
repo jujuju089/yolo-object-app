6bad7ce54f45b940b1b3804244805b5afe59ae64
jujuju089 <schmitju@katharineum.de>
2026-03-16 14:58:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def index():
     return render_template("index.html", result=result, filename=filename)
     
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False
+    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
